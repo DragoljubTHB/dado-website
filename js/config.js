6922d2d3a4a259e23f23cc40cd808516c4294665
dado <littlevwing@live.com>
2017-07-31 14:40:01 +0200

    simple html works login

---
+++ js/config.js
@@ -0,0 +1,9 @@
+var configConstants = {
+	auth0: {
+		domain: 'littlevwing.auth0.com',
+		clientId: 'j54pWi8fcQPC6eLy02v8jsIF_x9PgucP'
+	},
+  apiBaseUrl: 'https://qaov2snb71.execute-api.us-west-2.amazonaws.com/dev'
+};
+//https://us-west-2.console.aws.amazon.com/apigateway/home?region=us-west-2#/apis/qaov2snb71/stages/dev
+//https://qaov2snb71.execute-api.us-west-2.amazonaws.com/dev
