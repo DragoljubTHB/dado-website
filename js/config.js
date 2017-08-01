43208dce45efd29dd2db6b9b0fd23d1401be9ddb
dado <littlevwing@live.com>
2017-08-01 17:47:48 +0200

    minor

--- js/config.js
+++ js/config.js
@@ -6,29 +6,3 @@ var configConstants = {
   apiBaseUrl: 'https://qaov2snb71.execute-api.us-west-2.amazonaws.com/dev'
 };
 
-
-//id_token
-//eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImxpdHRsZXZ3aW5nQGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJwaWN0dXJlIjoiaHR0cHM6Ly9saDMuZ29vZ2xldXNlcmNvbnRlbnQuY29tLy1DbThNWnRmSnp1US9BQUFBQUFBQUFBSS9BQUFBQUFBQUFsYy9pTXpPMEpCdjF3dy9waG90by5qcGciLCJpc3MiOiJodHRwczovL2xpdHRsZXZ3aW5nLmF1dGgwLmNvbS8iLCJzdWIiOiJnb29nbGUtb2F1dGgyfDEwNjg1NzY5NTMxMTE2MDMwNjg2MSIsImF1ZCI6Imo1NHBXaThmY1FQQzZlTHkwMnY4anNJRl94OVBndWNQIiwiZXhwIjoxNTAxNTQyMDQ0LCJpYXQiOjE1MDE1MDYwNDR9.VrSxcFI1mrfLE_wLnUbUTI1h7ZILa6pRZNLgNf-eOTA
-//auth0 user info
-/*
-{"email":"littlevwing@gmail.com"
-,"email_verified":true
-,"name":"Dado Milasinovic"
-,"given_name":"Dado"
-,"family_name":"Milasinovic"
-,"picture":"https://lh3.googleusercontent.com/-Cm8MZtfJzuQ/AAAAAAAAAAI/AAAAAAAAAlc/iMzO0JBv1ww/photo.jpg"
-,"gender":"male"
-,"locale":"de"
-,"clientID":"j54pWi8fcQPC6eLy02v8jsIF_x9PgucP"
-,"updated_at":"2017-07-31T13:36:18.518Z"
-,"user_id":"google-oauth2|106857695311160306861"
-,"nickname":"littlevwing"
-,"identities":[
-	{"provider":"google-oauth2"
-	,"user_id":"106857695311160306861"
-	,"connection":"google-oauth2"
-	,"isSocial":true}
-			]
-,"created_at":"2017-07-24T13:09:40.184Z"
-,"global_client_id":"8867bsXDJTe6E08h5Y0TPK2W3cscKdhs"}
- */
